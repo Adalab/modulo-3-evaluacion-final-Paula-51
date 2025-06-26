392aead1f75121cd91d28bf6c3f3ac2f35c4ee6a
Paula <paulasanjosemartin@gmail.com>
2025-06-26 19:23:22 +0200

    fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './styles/App.scss'
 
 function App() {
   
+const [AllCharacters, setAllCharacters] = useState([])
 
+  useEffect(() => {
+  
+    fetch("https://hp-api.onrender.com/api/characters")
+      .then(response => response.json())
+      .then(data => {
+        setAllCharacters(data);
+      })
+  }, [])
   return (
     <>
     
