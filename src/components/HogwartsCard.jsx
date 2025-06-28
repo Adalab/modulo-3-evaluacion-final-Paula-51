ff0f6bad3b5b31f79e8f04975ffb780f6a249a55
Paula <paulasanjosemartin@gmail.com>
2025-06-28 13:26:17 +0200

    imagen por defecto

--- src/components/HogwartsCard.jsx
+++ src/components/HogwartsCard.jsx
@@ -1,13 +1,16 @@
 import '../styles/CardDetails.scss';
+import defaultImage from '../images/imgrelleno.png';
+
 
 function HogwartsCard({ character }) {
   return (
     <article className="hogwarts-card">
       <img
-        src={character.image || '/images/default.png'}
+        src={character.image ? character.image : defaultImage}
         alt={character.name}
         className="hogwarts-img"
       />
+
       <div className="hogwarts-info">
         <h3>{character.name}</h3>
         <p>{character.species}</p>
