a41aaf6aa24bf93cd06ae2773fd90a84c4ad7db5
Paula <paulasanjosemartin@gmail.com>
2025-07-01 14:15:24 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react()], base: "/modulo-3-evaluacion-final-Paula-51/",
+});
+
